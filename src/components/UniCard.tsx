5be557f95c62b97ea8b5e62ed5b3a05e695bd72b
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-16 22:53:08 +0100

    Added more styling to each component, reordered how society count works to include different text depending on number of societies.

--- src/components/UniCard.tsx
+++ src/components/UniCard.tsx
@@ -5,7 +5,7 @@ export default function UniCard( { university } : { university: any }) {
 
     return (
         <div className="border-2 border-gray-400 max-w-48 rounded-xl p-4 pt-2 m-10 flex flex-col items-center">
-            <h1 className="text-2xl">
+            <h1 className="text-2xl mb-2">
                 { university.name }
             </h1>
             <Image 
@@ -13,12 +13,13 @@ export default function UniCard( { university } : { university: any }) {
                 alt={"Logo for " + university.name}
                 width={100}
                 height={100}
+                className="m-1"
             />
-            <div>
-                { university.rating}
+            <div className="text-yellow-600">
+                {university.rating} / 5
             </div>
             <div>
-                Society count: {university.societyCount}
+                {university.societyCount} societ{university.societyCount == 1 ? 'y' : 'ies'}
             </div>
         </div>
     )
