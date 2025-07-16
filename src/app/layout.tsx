d36dd7cf601015000c679b8115b893c730c13aa5
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-16 22:50:28 +0100

    Added search box, with styling.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ export default function RootLayout({ children } : { children: React.ReactNode}) 
                     <a href='/' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>Home</a>
                     <a href='/universities' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>Universities</a>
                     <a href='/about' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>About</a>
+                
+                    <input type='text' placeholder='Search' className='ml-auto border-2 p-3 rounded-xl placeholder:text-black'/>
                 </nav>
                 {children}
             </body>
