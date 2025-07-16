0da158707afbdb13ac0611621bf53da756f7fcaa
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-17 00:27:03 +0100

    Ran HeadWind to reorder tailwind classnames.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,12 @@ export default function RootLayout({ children } : { children: React.ReactNode}) 
     return(
         <html lang="en">
             <body className='flex flex-col items-center'>
-                <nav className='flex bg-amber-500 p-5 drop-shadow-xl drop-shadow-zinc-300 w-full mb-20'>
-                    <a href='/' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>Home</a>
-                    <a href='/universities' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>Universities</a>
-                    <a href='/about' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>About</a>
+                <nav className='flex w-full p-5 mb-20 bg-amber-500 drop-shadow-xl drop-shadow-zinc-300'>
+                    <a href='/' className='p-3 mr-3 transition duration-300 border-2 rounded-xl hover:bg-blue-500 hover:text-white'>Home</a>
+                    <a href='/universities' className='p-3 mr-3 transition duration-300 border-2 rounded-xl hover:bg-blue-500 hover:text-white'>Universities</a>
+                    <a href='/about' className='p-3 mr-3 transition duration-300 border-2 rounded-xl hover:bg-blue-500 hover:text-white'>About</a>
                 
-                    <input type='text' placeholder='Search' className='ml-auto border-2 p-3 rounded-xl placeholder:text-black'/>
+                    <input type='text' placeholder='Search' className='p-3 ml-auto border-2 rounded-xl placeholder:text-black'/>
                 </nav>
                 {children}
             </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@
 
 export default function Home() {
     return(
-        <div className="text-center justify-center items-center w-1/2">
+        <div className="items-center justify-center w-1/2 text-center">
 
-            <h1 className="text-6xl mb-20">Rate My Society</h1>
+            <h1 className="mb-20 text-6xl">Rate My Society</h1>
 
             <div className="flex flex-col text-center border-2 p-15 rounded-2xl">
-                <h3 className="text-2xl mb-5">Search a University</h3>
+                <h3 className="mb-5 text-2xl">Search a University</h3>
                 <form>
                     <input type="text" placeholder="Search" className="mr-2 text-center" />
                 </form>
