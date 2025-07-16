b6bb7adbcad49dfde36cc67c65033378a70f9d05
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-16 23:04:08 +0100

    Added parent div with styling to keep items central.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css'
 export default function RootLayout({ children } : { children: React.ReactNode}) {
     return(
         <html lang="en">
-            <body>
-                <nav className='flex bg-amber-500 p-5 drop-shadow-xl drop-shadow-zinc-300'>
+            <body className='flex flex-col items-center'>
+                <nav className='flex bg-amber-500 p-5 drop-shadow-xl drop-shadow-zinc-300 w-full mb-20'>
                     <a href='/' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>Home</a>
                     <a href='/universities' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>Universities</a>
                     <a href='/about' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>About</a>
