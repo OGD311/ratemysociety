d9d9cfcab12713d8e108ee658196b66cb8b842ea
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-16 22:48:31 +0100

    Added navbar and links with styling.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ export default function RootLayout({ children } : { children: React.ReactNode}) 
     return(
         <html lang="en">
             <body>
+                <nav className='flex bg-amber-500 p-5 drop-shadow-xl drop-shadow-zinc-300'>
+                    <a href='/' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>Home</a>
+                    <a href='/universities' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>Universities</a>
+                    <a href='/about' className='border-2 p-3 rounded-xl hover:bg-blue-500 hover:text-white transition duration-300 mr-3'>About</a>
+                </nav>
                 {children}
             </body>
         </html>
