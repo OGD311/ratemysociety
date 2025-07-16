2423d0cd2d7b9a9039f72450396aa605bea2add8
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-16 22:40:39 +0100

    Added tailwind styling for overall div and for title. Change src name to lowercase for easier management, and added file extension. Changed format for image alt text to always work regardless of university name.

--- src/components/UniCard.tsx
+++ src/components/UniCard.tsx
@@ -4,13 +4,13 @@ export default function UniCard( { university } : { university: any }) {
 
 
     return (
-        <div>
-            <h1>
+        <div className="border-2 border-gray-400 max-w-48 rounded-xl p-4 pt-2 m-10 flex flex-col items-center">
+            <h1 className="text-2xl">
                 { university.name }
             </h1>
             <Image 
-                src={`/${(university.name).replace(" ", "_")}`}
-                alt={university.name + "logo"}
+                src={`/${(university.name).toLowerCase().replace(" ", "_")}.svg`}
+                alt={"Logo for " + university.name}
                 width={100}
                 height={100}
             />
