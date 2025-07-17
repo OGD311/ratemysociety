23b7f6c3b75286af6e52cf10123dce18901e001d
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-17 12:38:24 +0100

    Added generateUniName function to take in a url safe string and convert it to a nice title for display.

--- src/utils/URLSafe.ts
+++ src/utils/URLSafe.ts
@@ -1,3 +1,17 @@
 export function generateURLSafe(unsafe: string) {
     return unsafe.toLowerCase().replace(/ /g, "_")
 }
+
+export function generateUniName(safe: string): string {
+    let unsafe = safe.replace(/_/g, " ").split(" ")
+
+    unsafe.forEach((word: string, index: number) => {
+        if (word.toLowerCase() !== "of") {
+            unsafe[index] = word.substring(0, 1).toUpperCase() + word.substring(1);
+        }
+    });
+
+    safe = unsafe.join(" ");
+
+    return safe
+}
