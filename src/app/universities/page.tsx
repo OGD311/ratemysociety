0fe5ffe3be4f7db5186cf26b7a82d14e97c03377
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-17 21:38:20 +0100

    Added title to Universities page.

--- src/app/universities/page.tsx
+++ src/app/universities/page.tsx
@@ -3,6 +3,9 @@ import UniGrid from "@/components/university/UniGrid";
 export default function UniversitiesHome() {
     
     return(
+        <>
+        <h1  className="mb-10 text-5xl">Universities</h1>
         <UniGrid />
+        </>
     )
 }
