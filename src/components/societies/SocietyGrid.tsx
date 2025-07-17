134b688d066950e919056954858ef80b6ea063f8
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-17 20:46:52 +0100

    Tightened up styling, changing how gaps between cards work to make layout shift smaller, and allow for greater control over gap.

--- src/components/societies/SocietyGrid.tsx
+++ src/components/societies/SocietyGrid.tsx
@@ -35,7 +35,7 @@ export default function SocietyGrid({ universityId } : { universityId: number })
             {!loading && !error && societies.length === 0 && <p>No societies found.</p>}
 
             {!loading && !error && societies.length > 0 &&
-                <div className="grid grid-cols-4">
+                <div className="grid grid-cols-5 gap-10">
                     {societies.map( (society) => (
                         <SocietyCard key={society.id} society={society} />
                     ))}
