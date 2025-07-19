024ace7b501256576bebeb57c6be947606e0febe
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-19 16:50:30 +0100

    Created basic ReviewCard to display each review.

---
+++ src/components/reviews/ReviewCard.tsx
@@ -0,0 +1,15 @@
+import { REVIEW } from "@/constants/interfaces";
+
+
+export default function ReviewCard({ review } : { review: REVIEW }) {
+
+    return (
+        <div className="bg-gray-500 p-4 m-2">
+            <div className="flex justify-between">
+                <p>{ review.rating } / 5</p>
+                <p>Posted on { new Date(review.posted_at).toLocaleDateString('en-GB') }</p>
+            </div>
+            <p className="mt-3">{ review.comment }</p>
+        </div>
+    )
+}
