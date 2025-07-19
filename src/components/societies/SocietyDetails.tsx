d1a2c88aa7075ee4f872c68650194e6d7d7b4871
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-19 12:04:46 +0100

    Moved styling from society page into society details for easier editing and less style clashes.

--- src/components/societies/SocietyDetails.tsx
+++ src/components/societies/SocietyDetails.tsx
@@ -4,10 +4,11 @@ import { SOCIETY } from "@/constants/interfaces";
 export default function SocietyDetails( { society } : { society: SOCIETY }) {
 
     return (
-        <>
-        <h1>{society.name}</h1>
-        <p>{society.description}</p>
-        <p>{society.memberCount}</p>
-        </>
+        
+        <div className="border-2 p-4 rounded-xl w-[50dvw] min-w-[50dvw] max-w-[50dvw] h-64 min-h-64 max-h-64">    
+            <h1>{society.name}</h1>
+            <p>{society.description}</p>
+            <p>{society.memberCount}</p>
+        </div>
     )
 }
