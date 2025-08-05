9e6bf54c79d45bd6fdb40f232e25cc01c3ae51e3
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-05 17:56:16 +0100

    Added code to extract turnstile token from form, verify its existence before reaching the backend, and then supplying it with server calls.

--- src/components/reviews/ReviewForm.tsx
+++ src/components/reviews/ReviewForm.tsx
@@ -1,6 +1,4 @@
-
 import { submitReview } from '@/lib/reviews';
-import { redirect } from 'next/navigation';
 import React, { useState } from 'react';
 
 export default function ReviewForm({ societyId } : { societyId: number}) {
@@ -10,8 +8,16 @@ export default function ReviewForm({ societyId } : { societyId: number}) {
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const form = event.target as HTMLFormElement;
+        
         const rating = (form.elements.namedItem('rating') as HTMLInputElement)?.value;
         const review = (form.elements.namedItem('review') as HTMLInputElement)?.value;
+
+        const turnstileToken = (form.elements.namedItem('cf-turnstile-response') as HTMLInputElement)?.value;
+        
+        if (!turnstileToken) {
+            setError(true);
+            return;
+        }
 
         try {
             setLoading(true);
@@ -19,7 +25,7 @@ export default function ReviewForm({ societyId } : { societyId: number}) {
             await submitReview(societyId, {
                 "rating": parseInt(rating),
                 "comment": review
-            })
+            }, turnstileToken)
 
             window.location.reload();
         
@@ -32,6 +38,8 @@ export default function ReviewForm({ societyId } : { societyId: number}) {
     }
 
     return (
+        <>
+        <script src='https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit' />
         <form onSubmit={handleSubmit} className='flex flex-col w-2/3 mt-10'>
             <div>
                 <label>
@@ -45,8 +53,12 @@ export default function ReviewForm({ societyId } : { societyId: number}) {
                     <input type="text" name="review" maxLength={255} />
                 </label>
             </div>
+            
+            <div className="cf-turnstile" data-sitekey="0x4AAAAAABl8zl5ftj0cIyJj"></div>
+
             <button type="submit" disabled={loading}>{!loading && "Submit"}{loading && "Submitting..."}</button>
             {error && <p className='text-red-500'>Error submitting review - please try again</p>}
         </form>
+        </>
     );
 }
