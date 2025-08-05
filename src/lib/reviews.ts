b94f0d90bfafb8ed9878613c37ce902e0590390c
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-05 16:26:52 +0100

    Added function to add review to database.

--- src/lib/reviews.ts
+++ src/lib/reviews.ts
@@ -1,7 +1,6 @@
 'use server';
+import { REVIEW } from "@/constants/interfaces";
 import { prisma } from "./prisma";
-
-
 
 export const getReviews = async (societyId: number) => {
     return await prisma.review.findMany({
@@ -11,3 +10,14 @@ export const getReviews = async (societyId: number) => {
     })
 }
 
+export const submitReview = async (societyId: number, review: any) => {
+
+    return await prisma.review.create({
+        data: {
+            rating: review.rating,
+            comment: review.comment,
+            posted_at: review.posted_at,
+            societyId: societyId,
+        }
+    });
+}
