7a97470de8d099e41779395e20a9030dd0ae5f64
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-05 23:21:07 +0100

    Modified size, shape, and order of elements of SocietyDetails.

--- src/components/societies/SocietyDetails.tsx
+++ src/components/societies/SocietyDetails.tsx
@@ -7,7 +7,7 @@ export default function SocietyDetails( { society, universityName } : { society:
 
     return (
         
-        <div className="border-2 border-gray-200 shadow-lg bg-white p-6 rounded-2xl w-[25dvw] min-w-[300px] max-w-[400px] h-auto flex flex-col justify-between transition-transform hover:scale-105 duration-200">
+        <div className="border-2 border-gray-200 shadow-lg bg-white p-6 rounded-2xl w-[30dvw] min-w-[30dvw] max-w-[30dvw] h-auto flex flex-col justify-between transition-transform hover:scale-105 duration-200">
             <div>
             <h1 className="text-3xl font-bold mb-3 text-indigo-700">{society.name}</h1>
             {society.description ? (
@@ -18,11 +18,11 @@ export default function SocietyDetails( { society, universityName } : { society:
                 </p>
             )}
             </div>
-            <div className="flex items-center justify-between mt-6">
-            <div className="text-md text-gray-600">
+            <div className="flex flex-col mt-6">
+            <StarRating rating={society.rating} />
+            <div className="text-md mt-3 text-gray-600">
                 Around <span className="font-semibold">{society.memberCount}</span> member{society.memberCount == 1 ? '' : 's'}
             </div>
-            <StarRating rating={society.rating} />
             </div>
         </div>
     )
