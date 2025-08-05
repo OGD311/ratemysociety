78671a0d179499d5e2b6d416a92928b9aadce879
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-05 22:04:39 +0100

    Changed styling for review cards.

--- src/components/reviews/ReviewCard.tsx
+++ src/components/reviews/ReviewCard.tsx
@@ -4,7 +4,7 @@ import { REVIEW } from "@/constants/interfaces";
 export default function ReviewCard({ review } : { review: REVIEW }) {
 
     return (
-        <div className="bg-gray-500 p-4 m-2 w-auto min-w-3xs text-pretty break-words max-w-3xs h-auto">
+        <div className="bg-gray-400 p-4 m-2 text-pretty break-words min-h-50 max-h-50 h-50">
             <div className="flex justify-between">
                 <p>{ review.rating } / 5</p>
                 <p>Posted on { new Date(review.posted_at).toLocaleDateString('en-GB') }</p>
