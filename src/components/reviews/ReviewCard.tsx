ca2f11ece9058fa648e391171dfb31f4fa22ca77
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-05 21:29:23 +0100

    Added text wrap and word break to review for longer reviews.

--- src/components/reviews/ReviewCard.tsx
+++ src/components/reviews/ReviewCard.tsx
@@ -4,7 +4,7 @@ import { REVIEW } from "@/constants/interfaces";
 export default function ReviewCard({ review } : { review: REVIEW }) {
 
     return (
-        <div className="bg-gray-500 p-4 m-2 w-auto min-w-3xs">
+        <div className="bg-gray-500 p-4 m-2 w-auto min-w-3xs text-pretty break-words max-w-3xs h-auto">
             <div className="flex justify-between">
                 <p>{ review.rating } / 5</p>
                 <p>Posted on { new Date(review.posted_at).toLocaleDateString('en-GB') }</p>
