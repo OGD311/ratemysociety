d861a293f38976a18d421c41609c5913da63daa2
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-05 22:49:56 +0100

    Started to style SocietyDetails, with default message for if no society description is supplied.

--- src/components/societies/SocietyDetails.tsx
+++ src/components/societies/SocietyDetails.tsx
@@ -1,14 +1,21 @@
 import { SOCIETY } from "@/constants/interfaces";
+import StarRating from "../StarRating";
+import { UniNameArticle } from "@/utils/UniName";
 
 
-export default function SocietyDetails( { society } : { society: SOCIETY }) {
+export default function SocietyDetails( { society, universityName } : { society: SOCIETY, universityName: string }) {
 
     return (
         
-        <div className="border-2 p-4 rounded-xl w-[50dvw] min-w-[50dvw] max-w-[50dvw] h-64 min-h-64 max-h-64">    
-            <h1>{society.name}</h1>
-            <p>{society.description}</p>
-            <p>{society.memberCount}</p>
+        <div className="border-2 p-4 rounded-xl w-[25dvw] min-w-[25dvw] max-w-[25dvw] h-64 min-h-64 max-h-64">    
+            <h1 className="text-2xl mb-5">{society.name}</h1>
+            {society.description && <p>{society.description}</p>}
+            {!society.description && <p>{society.name} is a society of category {"category"} at {UniNameArticle(universityName)} {universityName}</p>}
+
+            <div className="mt-5 text-md">
+                Around {society.memberCount} member{society.memberCount == 1 ? '' : 's'}
+            </div>
+            <StarRating rating={society.rating} />
         </div>
     )
 }
