397ebd35490385bc233ca3545f9ede60ca731be9
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-07 00:02:27 +0100

    Removed unique constraint from category.

---
+++ prisma/migrations/20250806225948_removed_unique_category_constraint/migration.sql
@@ -0,0 +1,22 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Society" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "rating" REAL NOT NULL DEFAULT 0,
+    "memberCount" INTEGER NOT NULL DEFAULT 0,
+    "website" TEXT,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "universityId" INTEGER NOT NULL,
+    "categoryId" INTEGER,
+    CONSTRAINT "Society_universityId_fkey" FOREIGN KEY ("universityId") REFERENCES "University" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Society_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Society" ("categoryId", "description", "id", "memberCount", "name", "rating", "universityId", "updated_at", "website") SELECT "categoryId", "description", "id", "memberCount", "name", "rating", "universityId", "updated_at", "website" FROM "Society";
+DROP TABLE "Society";
+ALTER TABLE "new_Society" RENAME TO "Society";
+CREATE UNIQUE INDEX "Society_name_key" ON "Society"("name");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
