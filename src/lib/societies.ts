4964168272aef3c68640f5f66a0e7b9be7a3a302
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-06 23:31:35 +0100

    Fixed issue where page crashed if society had no category.

--- src/lib/societies.ts
+++ src/lib/societies.ts
@@ -24,7 +24,7 @@ export const getSocietyDetails = async (universityName: string, societyName: str
     const results: SOCIETY[] = await prisma.$queryRaw`
         SELECT s.*, c.name as category_name, c.colour as category_colour FROM Society s
         INNER JOIN University u ON s.universityId = u.id
-        INNER JOIN Category c ON s.categoryId = c.id
+        LEFT JOIN Category c ON s.categoryId = c.id
         WHERE LOWER(s.name) = LOWER(${societyName}) AND LOWER(u.name) = LOWER(${universityName})
         LIMIT 1
     `;
