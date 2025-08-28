9eb98377b02ee6eccb51c404899162472e1b7b9d
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-28 16:23:24 +0100

    Added call to updateSocietyCount to be ran on each server start, to ensure data is up to date.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+import { updateSocietyCount } from "@/lib/universities";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+module.exports = () => {
+  console.log('Running startup...')
+  console.log('Society Count: 🔄')
+  updateSocietyCount()
+  console.log('Society Count: ✅')
+  return nextConfig;
+};
