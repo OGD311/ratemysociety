b0690dee94bb19406ea5231fd8015a8f9baba62c
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-29 20:25:32 +0100

    Placeholder privacy, terms and conditions pages.

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,5 @@
+export default function Privacy() {
+    return (
+        <p>Privacy</p>
+    )
+}
---
+++ src/app/termsandconditons/page.tsx
@@ -0,0 +1,5 @@
+export default function TermsAndConditions() {
+    return (
+        <p>TermsAndConditions</p>
+    )
+}
